194f1a528b728dcd7d1884b11d5c96c0fa51668b
Dylan Caldwell <dylan.caldwell35@gmail.com>
2025-11-24 20:40:28 -0500

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Index from './components/Index';
-import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
 
 function App() {
